4164df8456e3e0879963cfd6212e5e44f78c585b
Brian Aranibar <aranibarbrian63@gmail.com>
2026-03-25 22:03:25 -0400

    Layout final con navegación por roles y botones de Login/Logout

---
+++ Practica5/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica5.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Introduce un formato de correo válido")]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Recordarme")]
+        public bool RememberMe { get; set; }
+    }
+}
